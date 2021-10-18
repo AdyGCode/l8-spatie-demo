7c23ca0d3b781cc272332bc8b7a6b0baefde60bf
Ady Gould <ady.g.coder@gmail.com>
2021-10-18 18:04:00 +0800

    Added Tailwind extras, FontAwesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     purge: [
@@ -13,6 +14,48 @@ module.exports = {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            // Build your palette here
+            transparent: 'transparent',
+            current: 'currentColor',
+            white: colors.white,
+            black: colors.black,
+            blueGray: colors.blueGray,
+            coolGray: colors.coolGray,
+            gray: colors.gray,
+            trueGray: colors.trueGray,
+            warmGray: colors.warmGray,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+
+            /* Alternative names for some colours */
+            primary: colors.blue,
+            secondary: colors.purple,
+            tertiary: colors.gray,
+            danger: colors.red,
+            warning: colors.amber,
+            info: colors.blueGray,
+            success: colors.green,
+        },
+        linearGradientColors: theme => theme('colors'),
+        radialGradientColors: theme => theme('colors'),
+        conicGradientColors: theme => theme('colors'),
+
     },
 
     variants: {
@@ -21,5 +64,12 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+        require('tailwindcss-gradients'),
+        require('tailwindcss-elevation')(['responsive']),
+    ],
 };
